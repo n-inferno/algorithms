b42e1e053fe461b7562da7a14b91c7215ff1fd43
n-inferno <marya.andrijanowa@yandex.ru>
2020-09-25 09:21:55 +0300

    added quick sort

---
+++ quick_sort.py
@@ -0,0 +1,11 @@
+def quick_sort(array):
+    """
+    функция быстрой сортировки по возрастанию
+    """
+    if len(array) < 2:
+        return array
+    pivot = array[0]
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+

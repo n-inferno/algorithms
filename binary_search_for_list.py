cf75b3601399b95b0e233c13c403e0cda88cf8a3
n-inferno <marya.andrijanowa@yandex.ru>
2021-05-05 11:53:45 +0300

    added recursive binary search

--- binary_search_for_list.py
+++ binary_search_for_list.py
@@ -1,9 +1,9 @@
 def binary_search(array, k):
     """бинарный поиск элемента в упорядоченном списке"""
     low = 0
-    high = len(array)-1
+    high = len(array) - 1
     while low <= high:
-        mid = low + (high-low)//2
+        mid = low + (high - low) // 2
         if array[mid] == k:
             return mid + 1
         elif array[mid] > k:
@@ -12,8 +12,20 @@ def binary_search(array, k):
             low = mid + 1
     return -1
 
-if __name__ == '__main__':
-    arr = list(map(int, input().split()))
-    keys = list(map(int, input().split()))
-    for key in keys:
-        print(binary_search(arr, key), end=' ')
+
+def binary_search_recursive(array, k, start=0, end=None):
+    """
+    бинарный поиск элемента в упорядоченном списке,
+    рекурсивный алгоритм
+    """
+    if end is None:
+        end = len(array) - 1
+    mid = start + (end - start) // 2
+    if array[mid] == k:
+        return mid
+    if start == end:
+        return -1
+    if k < array[mid]:
+        return binary_search_recursive(array, k, start, mid)
+    else:
+        return binary_search_recursive(array, k, mid + 1, end)
